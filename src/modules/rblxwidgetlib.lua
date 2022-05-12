23540c86adfd104a6020f302a43f485b24c3a675
CL <minytale16@gmail.com>
2022-05-11 20:26:53 -0700

    Forgot to make a repo, oops

---
+++ src/modules/rblxwidgetlib.lua
@@ -0,0 +1,121 @@
+local m = {}
+
+local plugin, name, widget, bgframe, scrollframe, scrollbg, listlayout
+
+-- self explanitory
+local ScrollbarIMG = {"http://www.roblox.com/asset/?id=9599518795",
+"http://www.roblox.com/asset/?id=9599545837",
+"http://www.roblox.com/asset/?id=9599519108"}
+local StudioColor = Enum.StudioStyleGuideColor
+
+-- dumps the gui into workspace for debugging
+function DumpGUI()
+    local temp = Instance.new("ScreenGui", workspace)
+    temp.Name = "Dump " .. os.time()
+    for _, i in pairs(widget:GetChildren()) do
+        i:Clone().Parent = temp
+    end
+end
+
+-- syncing element colors to studio theme colors
+function ColorSync(element, property, enum)
+    local function sync()
+        element[property] = settings().Studio.Theme:GetColor(enum)
+    end
+    settings().Studio.ThemeChanged:Connect(sync)
+	sync()
+end
+
+-- initalizing an automatically scaling scrolling frame in widget
+function initScrollframe()
+    -- scroll bar background
+    scrollbg = Instance.new("Frame", bgframe)
+    scrollbg.Size = UDim2.new(0,15,1,0)
+    scrollbg.Position = UDim2.new(1,-15,0,0)
+    scrollbg.Name = "ScrollbarBG"
+    scrollbg.ZIndex = 1
+    ColorSync(scrollbg, "BackgroundColor3", StudioColor.ScrollBarBackground)
+
+    -- scrolling frame
+    scrollframe = Instance.new("ScrollingFrame", bgframe)
+    scrollframe.BackgroundTransparency = 1
+    scrollframe.Size = UDim2.new(1,0,1,0)
+    scrollframe.ScrollBarThickness = 15
+    scrollframe.BottomImage, scrollframe.MidImage, scrollframe.TopImage = ScrollbarIMG[1], ScrollbarIMG[2], ScrollbarIMG[3]
+    scrollframe.ScrollingDirection = Enum.ScrollingDirection.Y
+    scrollframe.Name = "ScrollingFrame"
+    scrollframe.ZIndex = 2
+    ColorSync(scrollframe, "ScrollBarImageColor3", StudioColor.ScrollBar)
+    ColorSync(scrollframe, "BorderColor3", StudioColor.Border)
+
+    -- list layout for later elements
+    listlayout = Instance.new("UIListLayout", scrollframe)
+    listlayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+
+    -- updating the scrollingframe whenever things are added or the size of the widow is changed
+    listlayout.Changed:Connect(function(p)
+        if p == "AbsoluteContentSize" then UpdateFrameSize() end
+    end)
+    bgframe.Changed:Connect(function(p)
+        if p == "AbsoluteSize" then UpdateFrameSize() end
+    end)
+end
+
+-- updaing the scrolling frame to fit window size based on element size
+function UpdateFrameSize()
+    scrollframe.CanvasSize = UDim2.new(0,0,0,listlayout.AbsoluteContentSize.Y)
+    scrollbg.Visible = scrollframe.AbsoluteWindowSize.Y < scrollframe.AbsoluteCanvasSize.Y
+end
+
+-- initalizing the gui into widget
+function initFrame()
+    bgframe = Instance.new("Frame", widget)
+    ColorSync(bgframe, "BackgroundColor3", StudioColor.MainBackground)
+    bgframe.Size = UDim2.new(1,0,1,0)
+    bgframe.Name = "BGFrame"
+    bgframe.ZIndex = 0
+    initScrollframe()
+    DumpGUI()
+    UpdateFrameSize()
+end
+
+-- initalizing the entire widget along with gui
+function m.initWidget(p, n, title)
+    plugin = p
+    name = n
+    if not title then title = name end
+    local WidgetInfo = DockWidgetPluginGuiInfo.new(
+        Enum.InitialDockState.Float,
+	    false,
+	    false,
+	    200,
+	    200,
+	    150,
+        150
+    )
+    widget = plugin:CreateDockWidgetPluginGui(name, WidgetInfo)
+    widget.Title = title
+    initFrame()
+    return widget
+end
+
+-- creating a frame for elements
+function NewFrame()
+    local newFrame = Instance.new("Frame", scrollframe)
+    newFrame.BackgroundTransparency = 1
+    newFrame.Size = UDim2.new(1,0,0,30)
+    return newFrame
+end
+
+-- creating textboxes
+function m.NewTextbox(text, alignment)
+    local frame = NewFrame()
+    local newTextbox = Instance.new("TextLabel", frame)
+    newTextbox.BackgroundTransparency = 1
+    newTextbox.Size = UDim2.new(1,-20,1,0)
+    newTextbox.Position = UDim2.new(0,10,0,0)
+    newTextbox.TextXAlignment = Enum.TextXAlignment[alignment]
+    ColorSync(newTextbox, "TextColor3", )
+end
+
+return m
---
+++ src/plugin.client.lua
@@ -0,0 +1,34 @@
+--[[
+    [RBLXWidgetLib]
+    something#7597
+    todo:
+        Textbox
+        Sections
+        Button
+        Dropdown
+        Sliders
+        Toggle
+        Input windows
+        Keybinds
+        Alerts
+]]--
+
+-- dependencies
+local dep = require(script.Parent.dependencies)
+
+-- get userID(unnessecary)
+local playerId = game:GetService("StudioService"):GetUserId()
+
+-- generate widget
+local widget = dep.widgetlib.initWidget(plugin, "rblxwidgetlib", "rblxwidgetlib - " .. game:GetService("Players"):GetNameFromUserIdAsync(playerId))
+
+-- plugin toolbar
+toolbar = plugin:CreateToolbar("rblxwidgetlib")
+
+-- toggle toolbar button
+local b_toggle = toolbar:CreateButton("","open widget","")
+
+
+-- toggle widget
+b_toggle.Click:Connect(function() widget.Enabled = not widget.Enabled end)
+
