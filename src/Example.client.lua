3bca70dc100f8f38a8faeada4cb48f85a0eb1136
CL <minytale16@gmail.com>
2022-05-14 13:48:16 -0700

    added buttons

--- src/Example.client.lua
+++ src/Example.client.lua
@@ -27,10 +27,9 @@ local widget = widgetlib.initWidget(plugin, "rblxwidgetlib")
 -- toggle widget button
 b_toggle.Click:Connect(function() widget.Enabled = not widget.Enabled end)
 
--- new textbox: usage - (text, font, X Alignment)
+-- new textbox: usage - (text, font, alignment, Parent)
 widgetlib.NewTextbox("Welcome to rblxwidgetlib!", Enum.Font.SourceSansBold, Enum.TextXAlignment.Center)
 
-
-
+widgetlib.NewButton("hi")
 -- dumps the gui into workspace in case you need to debug (temporary)
 widgetlib.DumpGUI()
--- src/rblxwidgetlib.lua
+++ src/rblxwidgetlib.lua
@@ -13,6 +13,15 @@ local ScrollbarIMG = {"http://www.roblox.com/asset/?id=9599518795",
 "http://www.roblox.com/asset/?id=9599519108"}
 local StudioColor = Enum.StudioStyleGuideColor
 
+local ButtonIMG = {
+    default = "http://www.roblox.com/asset/?id=9622824630",
+    hover = "http://www.roblox.com/asset/?id=9622825658",
+    pressed = "http://www.roblox.com/asset/?id=9538564172",
+    bg = "http://www.roblox.com/asset/?id=9538563350"
+}
+
+local UDim2fill = UDim2.new(1,0,1,0)
+
 -- dumps the gui into workspace for debugging
 function m.DumpGUI()
     local temp = Instance.new("ScreenGui", workspace)
@@ -47,7 +56,7 @@ function initScrollframe()
     -- scrolling frame
     m.scrollframe = Instance.new("ScrollingFrame", m.bgframe)
     m.scrollframe.BackgroundTransparency = 1
-    m.scrollframe.Size = UDim2.new(1,0,1,0)
+    m.scrollframe.Size = UDim2fill
     m.scrollframe.ScrollBarThickness = 15
     m.scrollframe.BottomImage, m.scrollframe.MidImage, m.scrollframe.TopImage = ScrollbarIMG[1], ScrollbarIMG[2], ScrollbarIMG[3]
     m.scrollframe.ScrollingDirection = Enum.ScrollingDirection.Y
@@ -59,6 +68,7 @@ function initScrollframe()
 
     -- list layout for later elements
     listlayout = Instance.new("UIListLayout", m.scrollframe)
+    listlayout.SortOrder = Enum.SortOrder.LayoutOrder
     listlayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 
     -- updating the scrollingframe whenever things are added or the size of the widow is changed
@@ -81,7 +91,7 @@ end
 function initFrame()
     m.bgframe = Instance.new("Frame", m.widget)
     ColorSync(m.bgframe, "BackgroundColor3", StudioColor.MainBackground)
-    m.bgframe.Size = UDim2.new(1,0,1,0)
+    m.bgframe.Size = UDim2fill
     m.bgframe.Name = "bgframe"
     m.bgframe.ZIndex = 0
     initScrollframe()
@@ -108,10 +118,20 @@ function m.NewFrame(height, parent)
     newFrame.BackgroundTransparency = 1
     newFrame.Size = UDim2.new(1,0,0,height)
 
-    -- table layout (used for stacking multiple elements in one row)
-    local tablelayout = Instance.new("UITableLayout", newFrame)
-    tablelayout.FillEmptySpaceColumns, tablelayout.FillEmptySpaceRows = true, true
-    tablelayout.FillDirection = Enum.FillDirection.Horizontal
+    -- layout (used for stacking multiple elements in one row)
+    local gridlayout = Instance.new("UIGridLayout", newFrame)
+    gridlayout.SortOrder = Enum.SortOrder.LayoutOrder
+    gridlayout.FillDirection = Enum.FillDirection.Vertical
+
+    gridlayout.Changed:Connect(function(p)
+        if p == "AbsoluteCellCount" then
+            gridlayout.CellSize = UDim2.new(1/gridlayout.AbsoluteCellCount.X,0,1,0)
+        end
+    end)
+
+    -- padding for elements in frame
+    local padding = Instance.new("UIPadding", newFrame)
+    padding.PaddingBottom, padding.PaddingLeft, padding.PaddingRight, padding.PaddingTop = UDim.new(0,5), UDim.new(0,5), UDim.new(0,5), UDim.new(0,5)
     return newFrame
 end
 
@@ -119,16 +139,59 @@ end
 function m.NewTextbox(text, font, alignment, parent)
     if not alignment then alignment = Enum.TextXAlignment.Center end
     if not parent then parent = m.NewFrame() end
+    if not font then font = Enum.Font.SourceSans end
     local newTextbox = Instance.new("TextLabel", parent)
     newTextbox.BackgroundTransparency = 1
-    newTextbox.Size = UDim2.new(1,-20,1,0)
-    newTextbox.Position = UDim2.new(0,10,0,0)
+    newTextbox.Size = UDim2fill
+    --newTextbox.Size = UDim2.new(1,-20,1,0)
+    --newTextbox.Position = UDim2.new(0,10,0,0)
     newTextbox.TextXAlignment = alignment
     newTextbox.TextSize = 15
     newTextbox.Font = font
     newTextbox.Text = text
     ColorSync(newTextbox, "TextColor3", StudioColor.MainText)
+    return newTextbox
 end
 
+function m.NewButton(textbox, parent)
+    local text
+    if not parent then parent = m.NewFrame() end
+
+    -- creating a frame to hold the button
+    local buttonFrame = Instance.new("Frame", parent)
+    buttonFrame.BackgroundTransparency = 1
+    buttonFrame.Name = "ButtonFrame"
+    buttonFrame.Size = UDim2fill
+
+    -- set up a textbox for the button
+    if type(textbox) == "string" then
+        text = m.NewTextbox(textbox, nil, nil, buttonFrame)
+    else
+        text = textbox
+    end
+    text.ZIndex = 3
+
+    -- button image
+    local button = Instance.new("ImageButton", buttonFrame)
+    button.Size = UDim2fill
+    button.BackgroundTransparency = 1
+    button.Image, button.HoverImage, button.PressedImage = ButtonIMG.default, ButtonIMG.hover, ButtonIMG.pressed
+    button.ScaleType = Enum.ScaleType.Slice
+    button.SliceCenter = Rect.new(7,7,156,36)
+    button.Name = "Button"
+    button.ZIndex = 2
+
+    -- dark background for the button
+    local buttonbg = Instance.new("ImageLabel", buttonFrame)
+    buttonbg.Size = UDim2fill
+    buttonbg.BackgroundTransparency = 1
+    buttonbg.ImageTransparency = 0.9
+    buttonbg.ScaleType = Enum.ScaleType.Slice
+    buttonbg.SliceCenter = Rect.new(7,7,156,36)
+    buttonbg.Image = ButtonIMG.bg
+    buttonbg.Name = "ButtonBG"
+
+    return button
+end
 
 return m
