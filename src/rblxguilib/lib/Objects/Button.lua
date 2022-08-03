96a610c0f4ba9964d800d8d0ff9bd1255cfad403
CL <minytale16@gmail.com>
2022-08-03 00:11:47 -0700

    Fixed ButtonBackground

--- src/rblxguilib/lib/Objects/Button.lua
+++ src/rblxguilib/lib/Objects/Button.lua
@@ -8,7 +8,8 @@ setmetatable(Button,GUIObject)
 
 Button.Images = {
     border = "http://www.roblox.com/asset/?id=10460485555",
-    bg = "http://www.roblox.com/asset/?id=10460051857"
+    bg = "http://www.roblox.com/asset/?id=10460051857",
+    selected = "http://www.roblox.com/asset/?id=10460676787"
 }
 
 function Button:SetDisabled(State)
@@ -16,9 +17,11 @@ function Button:SetDisabled(State)
     if self.Disabled then
         self.CursorIcon = "rbxasset://SystemCursors/Forbidden"
         self.Button.ImageTransparency, self.Textbox.TextTransparency = 0.5, 0.5
+        self.Button.PressedImage = self.Images.border
     else
         self.CursorIcon = "rbxasset://SystemCursors/PointingHand"
         self.Button.ImageTransparency, self.Textbox.TextTransparency = 0, 0
+        self.Button.PressedImage = self.Images.selected
     end
 end
 
@@ -41,26 +44,19 @@ function Button.new(Textbox, Size, Disabled, Parent)
     self.ButtonFrame.BackgroundTransparency = 1
     self.ButtonFrame.Name = "ButtonFrame"
     self.ButtonFrame.Size = UDim2.new(1,0,1,0)
-    -- set up a textbox for the button
+
     self.Button = Instance.new("ImageButton", self.ButtonFrame)
 
-    self.ButtonBackground = Instance.new("ImageLabel", self.Button)
-    self.ButtonBackground.Name = "ButtonBackground"
-    self.ButtonBackground.BackgroundTransparency = 1
-    self.ButtonBackground.Size = UDim2.new(1,0,1,0)
-    self.ButtonBackground.Image = self.Images.bg
-    util.ColorSync(self.ButtonBackground, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBackground)
-    self.ButtonBackground.ScaleType = Enum.ScaleType.Slice
-    self.ButtonBackground.SliceCenter = Rect.new(7,7,156,36)
-
+    -- set up a textbox for the button
     if type(Textbox) == "string" then
-        self.TextboxTable = TextboxMod.new(Textbox, nil, nil, nil, self.Button)
+        self.TextboxTable = TextboxMod.new(Textbox, nil, nil, nil, self.ButtonFrame)
     else
         self.TextboxTable = Textbox
         Textbox:Move(self.Button, true)
     end
     self.Textbox = self.TextboxTable.Textbox
     self.Textbox.ZIndex = 1
+
     Size = util.GetScale(Size)
     if Size then self.Button.Size = UDim2.new(Size.Scale,Size.Offset,1,0)
     else
@@ -80,7 +76,16 @@ function Button.new(Textbox, Size, Disabled, Parent)
     self.Button.ScaleType = Enum.ScaleType.Slice
     self.Button.SliceCenter = Rect.new(7,7,156,36)
     self.Button.Name = "Button"
-    self.Button.ZIndex = 0
+
+    self.ButtonBackground = Instance.new("ImageLabel", self.Button)
+    self.ButtonBackground.Name = "ButtonBackground"
+    self.ButtonBackground.BackgroundTransparency = 1
+    self.ButtonBackground.Size = UDim2.new(1,0,1,0)
+    self.ButtonBackground.Image = self.Images.bg
+    util.ColorSync(self.ButtonBackground, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBackground)
+    self.ButtonBackground.ScaleType = Enum.ScaleType.Slice
+    self.ButtonBackground.SliceCenter = Rect.new(7,7,156,36)
+    self.ButtonBackground.ZIndex = 0
 
     self.Button.MouseMoved:Connect(function()
         _G.PluginObject:GetMouse().Icon = self.CursorIcon
@@ -92,17 +97,6 @@ function Button.new(Textbox, Size, Disabled, Parent)
         _G.PluginObject:GetMouse().Icon = "rbxasset://SystemCursors/Arrow"
         if self.Disabled then return end
         util.ColorSync(self.Button, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBorder)
-        util.ColorSync(self.ButtonBackground, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBackground)
-    end)
-
-    
-    self.Button.MouseButton1Down:Connect(function()
-        if self.Disabled then return end
-        util.ColorSync(self.ButtonBackground, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBorder, Enum.StudioStyleGuideModifier.Selected)
-    end)
-    self.Button.MouseButton1Up:Connect(function()
-        if self.Disabled then return end
-        util.ColorSync(self.ButtonBackground, "ImageColor3", Enum.StudioStyleGuideColor.InputFieldBackground)
     end)
 
     self:SetDisabled(Disabled)
